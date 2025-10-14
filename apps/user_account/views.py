6b987b8646a15c26dda20db1d728ea597f206e92
Nathaniel Conduah <nathanielguggisberg@gmail.com>
2025-10-14 12:06:18 +0000

    Fix indentation error in register_view function

--- apps/user_account/views.py
+++ apps/user_account/views.py
@@ -59,11 +59,11 @@ def login_view(request):
 def register_view(request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-        user = serializer.save()
-        return Response(
-            {"message": "User created successfully"}, status=status.HTTP_201_CREATED
-        )
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            user = serializer.save()
+            return Response(
+                {"message": "User created successfully"}, status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["POST"])
